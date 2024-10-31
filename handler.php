4efdebe9ab0913e4e87cec1c9108e5375908582c
brianna-oregan <brianna.oregan@stu.ocsb.ca>
2024-10-31 14:38:05 +0000

    add welcome page

--- handler.php
+++ handler.php
@@ -1,22 +1,27 @@
 <!DOCTYPE html>
 <html>
+
   <head>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.deep_orange-light_blue.min.css">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <title>Welcome Page</title>
   </head>
 
   <body>
-    
+
     <?php
-    $userName = "";
-    if ( isset( $_POST['userName'] ) ){
-      $userName = $_POST['userName'];
+    $username = ""; // Make sure this is the variable you use.
+    if (isset($_POST['Username'])) { // Change 'username' to 'Username' (uppercase)
+    $username = $_POST['Username']; // Use the same case as in the form
     }
-    echo "<h1>Welcome ".$userName."!</h1>\n";
+    echo "<h1>Welcome, " . htmlspecialchars($username) . "!</h1>"; // Added htmlspecialchars for safety
     ?>
-    
+
+    <a href="index.html" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+      Go back
+    </a>
+
   </body>
-  
+
 </html>
